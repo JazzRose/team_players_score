006e1554292fdb7a5712b1a57c10257ef6807706
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-04-04 15:14:46 +0100

    first permit again

---
+++ single_class_lab_start_code/team_class/src/team.py
@@ -0,0 +1,20 @@
+class Team:
+    def __init__(self, input_name, input_players, input_coach):
+        self.name = input_name
+        self.players = input_players
+        self.coach = input_coach
+        self.points = 0
+
+    def add_player(self, player):
+        self.players.append(player)
+
+    def has_player(self, player):
+        for person in self.players:
+            if person == player:
+                return True
+        return False
+
+
+    def play_game(self, win):
+        if win == True :
+            self.points += 3
